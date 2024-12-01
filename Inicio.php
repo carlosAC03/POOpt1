a600f85089006c7db22ceb668c5d24a46bc4ce5e
carlosAC03 <cacostac03@educarex.es>
2024-12-01 13:26:09 +0100

    i

--- Inicio.php
+++ Inicio.php
@@ -4,12 +4,6 @@ include_once "CintaVideo.php";
 include_once "Dvd.php";
 include_once "Juego.php";
 
-$soporte = new Soporte("Tenet", 22, 3); // Esto fallará si Soporte es abstracta
-echo "<strong>" . $soporte->titulo . "</strong><br>";
-echo "Precio: " . $soporte->getPrecio() . " euros<br>";
-echo "Precio con IVA: " . $soporte->getPrecioConIVA() . " euros<br>";
-$soporte->muestraResumen();
-
 // Prueba de CintaVideo
 $cinta = new CintaVideo("Los Cazafantasmas", 1, 3.5, 120);
 $cinta->muestraResumen();
